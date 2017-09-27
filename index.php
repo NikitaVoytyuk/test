64299bc6181dd90c6dc57f9d9341cd5d985201ab
Nikita Voytyuk <nikitavoytyuk@gmail.com>
2017-09-27 17:31:37 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+class Animal{
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        echo $this->name;
+    }
+}
+
+Class Cat extends Animal{
+
+
+    public function __construct($name)
+    {
+        parent::__construct($name);
+    }
+
+    public function meow(){
+
+        echo "Cat $this->name is saying meow";
+    }
+
+}
+
+
+$cat = new Cat('garfield');
+$cat->getName();
+echo '<br>';
+$cat->meow();
